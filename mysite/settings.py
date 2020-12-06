413dfc1aff42a2131f0b22f6f5db00f4e2eef1f7
Jonibek <boronovjonibek@gmail.com>
2020-12-06 10:18:28 +0500

    'pybo'

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATICFILES_STORAGE = 'whitenoise.storage,CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
